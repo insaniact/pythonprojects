3cf5c6fb9770e5fb1212d609b52cea8c2e721d1d
insaniact <feelgoodnow365@gmail.com>
2024-05-15 12:12:16 -0500

    madlib

---
+++ madlib.py
@@ -0,0 +1,15 @@
+one = input('Enter a adjective: ')
+two = input('Enter a plural noun: ')
+three = input('Enter a plural noun: ')
+four = input('Enter a plural noun: ')
+five = input('Enter a plural noun: ')
+six = input('Enter a noun: ')
+seven = input('Enter an adverb: ')
+eight = input('Enter a noun: ')
+
+
+
+print ('The boys can watch an hour of ' +one+ ' television before turning off the ' +two+ ' in their room.')
+print('Make sure they do not watch any violent ' +three+ ' or adult ' +four+ '. If there are any phone ' +five+ ',')
+print('do not identify yourself as the ' +six+ '-sitter. Take a message. Write it ' +seven+ ' on the ' +eight+ '.')
+    
